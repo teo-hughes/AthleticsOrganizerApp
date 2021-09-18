fd3c47f6e56f84a3ce981b5a6154b362fe48ad6b
Teo <hughes.t@etoncollege.org.uk>
2021-09-18 12:21:14 +0100

    Added a MenuView

---
+++ AthleticsOrganizerApp/MenuView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  AthleticsOrganizerApp
+//
+//  Created by Hughes, Teo (BJH) on 18/09/2021.
+//
+
+// Test
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
