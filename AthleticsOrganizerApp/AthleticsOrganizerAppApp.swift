75d3370561dd8b694dc133f637bd8474eafd81f1
Teo <hughes.t@etoncollege.org.uk>
2021-09-18 12:04:22 +0100

    Initial Commit

---
+++ AthleticsOrganizerApp/AthleticsOrganizerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AthleticsOrganizerAppApp.swift
+//  AthleticsOrganizerApp
+//
+//  Created by Hughes, Teo (BJH) on 18/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct AthleticsOrganizerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
