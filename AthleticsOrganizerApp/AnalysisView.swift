2ad74461590fe4313be6b4fb60a3d608f7afca47
teo hughes <hughes.t@etoncollege.org.uk>
2021-09-22 18:55:51 +0100

    Added a Analysis View

---
+++ AthleticsOrganizerApp/AnalysisView.swift
@@ -0,0 +1,21 @@
+//
+//  AnalysisView.swift
+//  AthleticsOrganizerApp
+//
+//  Created by Teo Hughes on 18/09/2021.
+//
+
+import SwiftUI
+
+struct AnalysisView: View {
+    var body: some View {
+        Text("Analysis View")
+            .padding()
+    }
+}
+
+struct AnalysisView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnalysisView()
+    }
+}
