56c2ea7f4709aa80a6be7dfab06ba3dcd27b2bfb
Teo <hughes.t@etoncollege.org.uk>
2021-09-30 10:36:44 +0100

    Created an Info View

---
+++ AthleticsOrganizerApp/InfoView.swift
@@ -0,0 +1,21 @@
+//
+//  InfoView.swift
+//  AthleticsOrganizerApp
+//
+//  Created by Teo Hughes on 18/09/2021.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text("Info View")
+            .padding()
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView()
+    }
+}
