9628a8f8ba91ab7f5cf991f8e14ffda42c524032
Teo <hughes.t@etoncollege.org.uk>
2021-09-30 10:26:47 +0100

    Created a tab bar to connect all the different views

--- AthleticsOrganizerApp/MenuView.swift
+++ AthleticsOrganizerApp/MenuView.swift
@@ -9,9 +9,21 @@ import SwiftUI
 
 struct MenuView: View {
     var body: some View {
-        TabView {
-            MenuView()
-            AnalysisView()
+        
+        GeometryReader { geometry in
+            VStack{
+                Spacer()
+                Text("Tournaments")
+                Spacer()
+                HStack {
+                    TabBarIcon(width: geometry.size.width/3, height: geometry.size.height/28, systemIconName: "house", tabName: "Tournaments")
+                    TabBarIcon(width: geometry.size.width/3, height: geometry.size.height/28, systemIconName: "person.crop.circle", tabName: "Your Analysis")
+                    TabBarIcon(width: geometry.size.width/3, height: geometry.size.height/28, systemIconName: "info.circle", tabName: "Info")
+                }
+                .frame(width: geometry.size.width, height: geometry.size.height/8)
+                .background(Color("TabBarBackground").shadow(radius: 2))
+            }
+            .edgesIgnoringSafeArea(.bottom)
         }
     }
 }
@@ -19,5 +31,26 @@ struct MenuView: View {
 struct MenuView_Previews: PreviewProvider {
     static var previews: some View {
         MenuView()
+    }
+}
+
+struct TabBarIcon: View {
+    
+    let width, height: CGFloat
+    let systemIconName, tabName: String
+    
+    
+    var body: some View {
+        VStack{
+            Image(systemName: systemIconName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: width, height: height)
+                .padding(.top, 10)
+            Text(tabName)
+                .font(.footnote)
+            Spacer()
+        }
+        .padding(.horizontal, -2)
     }
 }
