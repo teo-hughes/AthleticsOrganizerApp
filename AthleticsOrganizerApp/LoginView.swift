d77d4532b58293ac44046ca0e3c8c334079a973c
Teo <hughes.t@etoncollege.org.uk>
2021-10-02 12:01:32 +0100

    Added A LoginView

---
+++ AthleticsOrganizerApp/LoginView.swift
@@ -0,0 +1,21 @@
+ //
+ //  LoginView.swift
+ //  AthleticsOrganizerApp
+ //
+ //  Created by Teo Hughes on 18/09/2021.
+ //
+
+ import SwiftUI
+
+ struct LoginView: View {
+     var body: some View {
+         Text("Login View")
+             .padding()
+     }
+ }
+
+ struct LoginView_Previews: PreviewProvider {
+     static var previews: some View {
+         LoginView()
+     }
+ }
