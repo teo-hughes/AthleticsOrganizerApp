5b6c6c3c1e961091ae6dd33a417ec708e3522ff8
Teo <hughes.t@etoncollege.org.uk>
2021-10-02 12:17:27 +0100

    Created a login view set up

--- AthleticsOrganizerApp/AthleticsOrganizerAppApp.swift
+++ AthleticsOrganizerApp/AthleticsOrganizerAppApp.swift
@@ -12,13 +12,14 @@ struct AthleticsOrganizerAppApp: App {
     
     
     // Menu View is the opening View so I need its parameters
-    @StateObject var viewOrganizer = ViewOrganizer()
+    //@StateObject var viewOrganizer = ViewOrganizer()
     
     var body: some Scene {
         WindowGroup {
             
             // Opens Menu View First
-            MenuView(viewOrganizer: viewOrganizer)
+            //MenuView(viewOrganizer: viewOrganizer)
+            LoginView()
         }
     }
 }
--- AthleticsOrganizerApp/LoginView.swift
+++ AthleticsOrganizerApp/LoginView.swift
@@ -8,9 +8,46 @@
  import SwiftUI
 
  struct LoginView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
      var body: some View {
-         Text("Login View")
-             .padding()
+        NavigationView {
+            VStack {
+                Image("TempLoginImage")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+                
+                VStack {
+                    TextField("Email Address", text: $email)
+                        .padding()
+                        .background(Color(.secondarySystemBackground))
+                        
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .background(Color(.secondarySystemBackground))
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Sign In")
+                            .foregroundColor(Color.white)
+                            .frame(width: 200, height: 50)
+                            .cornerRadius(8)
+                            .background(Color.blue)
+                            
+                    })
+                        
+                }
+                .padding()
+                Spacer()
+            }
+            .navigationTitle("Sign in")
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+        
      }
  }
 
