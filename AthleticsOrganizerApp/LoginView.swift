d6df229dde6e11cda4cac891a97ea433e696de9c
teo <hughes.t@etoncollege.org.uk>
2021-10-07 19:12:29 +0100

    Added Create AccountFunction and now authentication works

--- AthleticsOrganizerApp/LoginView.swift
+++ AthleticsOrganizerApp/LoginView.swift
@@ -29,6 +29,7 @@
             }
             
         }
+        
     }
     
     func signUp(email: String, password: String) {
@@ -41,6 +42,12 @@
                 self?.signedIn = true
             }
         }
+    }
+    
+    func signOut() {
+        try? auth.signOut()
+        
+        self.signedIn = false
     }
 
     
@@ -55,7 +62,21 @@
      var body: some View {
         NavigationView {
             if viewModel.signedIn {
-                Text("You are signed in")
+                VStack {
+                    Text("You are signed in")
+                    
+                    Button(action: {
+                        viewModel.signOut()
+                    }, label: {
+                        Text("Sign Out")
+                            .foregroundColor(Color.white)
+                            .frame(width: 200, height: 50)
+                            .cornerRadius(8)
+                            .background(Color.blue)
+                    })
+                    
+                }
+                
             } else {
                 SignInView()
             }
@@ -85,10 +106,14 @@
                 
             VStack {
                 TextField("Email Address", text: $email)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
                     .padding()
                     .background(Color(.secondarySystemBackground))
                         
                 SecureField("Password", text: $password)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
                     .padding()
                     .background(Color(.secondarySystemBackground))
                     
@@ -108,15 +133,69 @@
                         .background(Color.blue)
                             
                 })
+                
+                NavigationLink("Create Account", destination: SignUpView())
+                    .padding()
                         
             }
             .padding()
             Spacer()
         }
-        .navigationTitle("Sign in")
+        .navigationTitle("Sign In")
      }
  }
 
+ struct SignUpView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
+    @EnvironmentObject var viewModel: AppViewModel
+    
+     var body: some View {
+        VStack {
+            Image("TempLoginImage")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150, height: 150)
+                
+            VStack {
+                TextField("Email Address", text: $email)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                        
+                SecureField("Password", text: $password)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                    
+                Button(action: {
+                        
+                    guard !email.isEmpty, !password.isEmpty else {
+                        return
+                    }
+                        
+                    viewModel.signUp(email: email, password: password)
+                        
+                }, label: {
+                    Text("Create Account")
+                        .foregroundColor(Color.white)
+                        .frame(width: 200, height: 50)
+                        .cornerRadius(8)
+                        .background(Color.blue)
+                            
+                })
+                        
+            }
+            .padding()
+            Spacer()
+        }
+        .navigationTitle("Create Account")
+     }
+ }
 
  struct LoginView_Previews: PreviewProvider {
      static var previews: some View {
