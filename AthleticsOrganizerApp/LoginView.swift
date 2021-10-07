38958d5ba7ae051708b26bbccaeba456ada8315c
teo-hughes <hughes.t@etoncollege.org.uk>
2021-10-07 10:34:58 +0100

    Cleaned up Who is signed in when

--- AthleticsOrganizerApp/LoginView.swift
+++ AthleticsOrganizerApp/LoginView.swift
@@ -12,22 +12,33 @@
     
     let auth = Auth.auth()
     
+    @Published var signedIn = false
+    
     var isSignedIn: Bool {
         return auth.currentUser != nil
     }
     
     func signIn(email: String, password: String) {
-        auth.signIn(withEmail: email, password: password) { result, error in
+        auth.signIn(withEmail: email, password: password) { [weak self] result, error in
             guard result != nil, error == nil else {
                 return
             }
+            
+            DispatchQueue.main.async {
+                self?.signedIn = true
+            }
+            
         }
     }
     
     func signUp(email: String, password: String) {
-        auth.createUser(withEmail: email, password: password) { result, error in
+        auth.createUser(withEmail: email, password: password) { [weak self] result, error in
             guard result != nil, error == nil else {
                 return
+            }
+            
+            DispatchQueue.main.async {
+                self?.signedIn = true
             }
         }
     }
@@ -43,10 +54,17 @@
     
      var body: some View {
         NavigationView {
-            SignInView()
+            if viewModel.signedIn {
+                Text("You are signed in")
+            } else {
+                SignInView()
+            }
+            
         }
         .navigationViewStyle(StackNavigationViewStyle())
-        
+        .onAppear {
+            viewModel.signedIn = viewModel.isSignedIn
+        }
      }
  }
  
