fda1c31847e021be8207bc0fd45215b4dd9573fa
Teo <hughes.t@etoncollege.org.uk>
2021-10-25 11:50:55 +0100

    Added 2 new drop down menus in event view which determine which gender and ageGroup you want to see

--- AthleticsOrganizerApp/Views/EventView.swift
+++ AthleticsOrganizerApp/Views/EventView.swift
@@ -14,12 +14,84 @@ struct EventView: View {
     @State var tournamentAthletes: [Athlete]
     
     @State private var presentAddNewAthletesScreen = false
+    @State private var chosenGender: String = "Male"
+    @State private var chosenAgeGroup: String = ""
+    @State private var genderExpand: Bool = false
+    @State private var ageGroupExpand: Bool = false
     
     // The body of the InfoView
     var body: some View {
         
+
         VStack {
             
+            HStack {
+                VStack(spacing: 10) {
+                    HStack {
+                        Text(chosenGender)
+                            .fontWeight(.bold)
+                            .foregroundColor(.black)
+                        Image(systemName: genderExpand ? "chevron.up": "chevron.down")
+                            .resizable().frame(width: 13, height: 6)
+                            .foregroundColor(.black)
+                        
+                    }.onTapGesture {
+                        self.genderExpand.toggle()
+                    }
+                    if genderExpand {
+                        Button(action: {
+                            self.genderExpand.toggle()
+                            chosenGender = "Male"
+                        }, label: {
+                            Text("Male")
+                                .padding()
+                                .foregroundColor(.black)
+                        })
+                        Button(action: {
+                            self.genderExpand.toggle()
+                            chosenGender = "Female"
+                        }, label: {
+                            Text("Female")
+                                .padding()
+                                .foregroundColor(.black)
+                        })
+                    }
+                }
+                .padding()
+                
+                
+                Spacer()
+                
+                
+                VStack(spacing: 10) {
+                    HStack {
+                        Text(chosenAgeGroup)
+                            .fontWeight(.bold)
+                            .foregroundColor(.black)
+                        Image(systemName: ageGroupExpand ? "chevron.up": "chevron.down")
+                            .resizable().frame(width: 13, height: 6)
+                            .foregroundColor(.black)
+                        
+                    }.onTapGesture {
+                        self.ageGroupExpand.toggle()
+                    }
+                    if ageGroupExpand {
+                        ForEach(event.age_groups, id: \.self) { ageGroup in
+                            Button(action: {
+                                self.ageGroupExpand.toggle()
+                                chosenAgeGroup = ageGroup
+                            }, label: {
+                                Text(ageGroup)
+                                    .padding()
+                                    .foregroundColor(.black)
+                            })
+                        }
+                    }
+                }
+
+            }
+            .padding()
+
             Spacer()
             Text("Add Times")
             Button( action: { presentAddNewAthletesScreen.toggle() }, label: {
@@ -30,6 +102,9 @@ struct EventView: View {
         .sheet(isPresented: $presentAddNewAthletesScreen) {
             AddTimesView()
         }
+        .onAppear(perform: {
+            chosenAgeGroup = event.age_groups[0]
+        })
     }
 }
 
