09c1969c7b9f984d52580d547d561a35e7963829
Teo <hughes.t@etoncollege.org.uk>
2021-10-26 17:30:42 +0100

    finished athletesSearchView

--- AthleticsOrganizerApp/Views/SearchAthletesView.swift
+++ AthleticsOrganizerApp/Views/SearchAthletesView.swift
@@ -22,7 +22,11 @@ struct SearchAthletesView: View {
     @State var chosenAgeGroup: String
     @State var chosenGender: String
     
+    @Environment(\.presentationMode) var presentationMode
+    
     @State var AthleteNames: [String] = []
+    @State var AthletesChecked: [Bool] = []
+    @State var AthleteIndexes: [Int] = []
     
     
     @State var searchingFor = ""
@@ -32,20 +36,59 @@ struct SearchAthletesView: View {
 
         NavigationView {
             List {
-                ForEach(results, id: \.self) { athlete in
-                    NavigationLink(destination: Text(athlete)) {
-                        Text(athlete)
+                ForEach(0..<results.count, id: \.self) { n in
+                    
+                    let index: Int = AthleteNames.firstIndex(of: results[n])!
+                    HStack {
+                        Text(results[n])
+                        Spacer()
+                        ZStack {
+                            Image(systemName: "circle")
+                                .font(.system(size: 25))
+                            
+                            if AthletesChecked[index] {
+                                Image(systemName: "checkmark.circle.fill")
+                                    .font(.system(size: 25))
+                                    .foregroundColor(Color.green)
+                            }
+                        }
+                        
+                        
+                    }
+                    .padding()
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        AthletesChecked[index].toggle()
                     }
                 }
             }
             .searchable(text: $searchingFor)
             .navigationTitle("Add Athletes")
-            .navigationViewStyle(StackNavigationViewStyle())
+            .navigationBarItems(
+                leading: Button(action: {
+                    handleCancelTapped()
+                }, label: {
+                    Text("Cancel")
+                }),
+                trailing: Button(action: {
+                    
+                    for n in 0..<AthletesChecked.count {
+                        if AthletesChecked[n] {
+                            event.Athletes.append(tournamentAthletes[AthleteIndexes[n]])
+                        }
+                    }
+                    handleDoneTapped()
+                }, label: {
+                    Text("Done")
+                })
+            )
         }
         .onAppear(perform: {
-            for athlete in tournamentAthletes {
-                if athlete.age_group == chosenAgeGroup && athlete.gender == chosenGender {
-                    AthleteNames.append(athlete.name)
+            for n in 0..<tournamentAthletes.count {
+                if tournamentAthletes[n].age_group == chosenAgeGroup && tournamentAthletes[n].gender == chosenGender {
+                    AthleteNames.append(tournamentAthletes[n].name)
+                    AthletesChecked.append(false)
+                    AthleteIndexes.append(n)
                 }
             }
         })
@@ -57,5 +100,17 @@ struct SearchAthletesView: View {
         } else {
             return AthleteNames.filter { $0.contains(searchingFor)}
         }
+    }
+    
+    func handleCancelTapped() {
+        dismiss()
+    }
+    
+    func handleDoneTapped() {
+        dismiss()
+    }
+    
+    func dismiss() {
+        presentationMode.wrappedValue.dismiss()
     }
 }
